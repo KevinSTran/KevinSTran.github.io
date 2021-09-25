3f68964228a3c01c198972fb8788468ccee03614
Kevin S. Tran <tanksense@gmail.com>
2021-09-25 19:29:13 +1000

    Added Cyclone Assets and completed minimal restructuring.

---
+++ Cyclone/main.js
@@ -0,0 +1,3 @@
+export function Start() { console.log("MAH"); }
+export function Update() { console.log("HOW DID THAT ALIEN GET IN HERE"); }
+export function End() { console.log("I SAID THERE IS AN ALIEN BUT YOU DON'T EVER LISTEN TO ME"); }
