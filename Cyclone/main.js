62d25edd33e7ef57fde67ce3e6538bf96180b430
Kevin S. Tran <tanksense@gmail.com>
2021-09-28 08:09:28 +1000

    Experimenting with "default"

--- Cyclone/main.js
+++ Cyclone/main.js
@@ -1,3 +1,3 @@
-export function Start() { console.log("MAH"); }
-export function Update() { /*Main code goes here*/ }
-export function End() { console.log("I SAID THERE IS AN ALIEN BUT YOU DON'T EVER LISTEN TO ME"); }
+export default function Start() { console.log("MAH"); }
+export default function Update() { /*Main code goes here*/ }
+export default function End() { console.log("I SAID THERE IS AN ALIEN BUT YOU DON'T EVER LISTEN TO ME"); }
