91ecaf8d8788229e40b4ebf02bd1405b3c2d3b5d
Kevin S. Tran <tanksense@gmail.com>
2021-09-28 19:08:09 +1000

    Added print statement for analysis.

--- Cyclone/Systems/SampleSystem.js
+++ Cyclone/Systems/SampleSystem.js
@@ -3,6 +3,7 @@ export var sampleSystem =
 	requirements : [ "sampleComponent","transform" ],
 	Update : function(entity)
 	{
+		console.log(entity);
 		entity.transform.rotation.y += entity.sampleComponent.speed;
 	}
 }
