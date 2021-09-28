97d032d63e7d3164e6427779e45d5b4dc52c59fa
Kevin S. Tran <tanksense@gmail.com>
2021-09-28 17:18:10 +1000

    Array issue fixed.

--- Cyclone/index.js
+++ Cyclone/index.js
@@ -3,13 +3,13 @@ import { sampleEntity } from "./Entities/Actors/Characters/SampleEntity.js";
 import { sampleSystem } from "./Systems/SampleSystem.js";
 
 const entities = 
-{
+[
 	sampleEntity
-};
+];
 const systems =
-{
+[
 	sampleSystem
-};
+];
 
 export const module = 
 {
