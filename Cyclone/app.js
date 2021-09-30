a71494ebce2efa78f0f1b97c3454ac894f6cdf2f
Kevin S. Tran <tanksense@gmail.com>
2021-09-30 11:10:45 +1000

    "run" error fixed.

--- Cyclone/app.js
+++ Cyclone/app.js
@@ -14,6 +14,6 @@ const library =
 	meta
 };
 
-const Run = function() { machine.run(module, library); }
+const Run = function() { machine.Run(module, library); }
 
 export const app = { module, library, Run };
